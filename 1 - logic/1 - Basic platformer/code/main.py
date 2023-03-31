c0b701e5b7952157ffa484f486c8f3c9356f6b79
dreamcatstudios <102027407+dreamcatstudios@users.noreply.github.com>
2023-03-31 17:08:42 +0530

    Update main.py

--- 1 - logic/1 - Basic platformer/code/main.py
+++ 1 - logic/1 - Basic platformer/code/main.py
@@ -2,19 +2,28 @@ import pygame, sys
 from settings import * 
 from level import Level
 
+
 # Pygame setup
 pygame.init()
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
 level = Level(level_map,screen)
+white = (255,255,255)
+black = (0,0,0)
+color = black
 
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
-	
-	screen.fill('black')
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_u:
+				color = white
+
+			if event.key == pygame.K_y:
+				color = black
+	screen.fill(color)
 	level.run()
 
 	pygame.display.update()
