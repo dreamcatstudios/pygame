1cbaa6812346fc7055591d85bf4068f5d757f8e9
dreamcatstudios <102027407+dreamcatstudios@users.noreply.github.com>
2023-04-03 17:02:38 +0530

    Update settings.py

--- 1 - logic/1 - Basic platformer/code/settings.py
+++ 1 - logic/1 - Basic platformer/code/settings.py
@@ -2,17 +2,17 @@ level_map = [
 '                            ',
 '                            ',
 '                            ',
-' XX    YYYY          XX    ',
-' XX P                       ',
-' XXXX         XX         XX ',
-' XYYY    XX              ',
-' XX    X  XXXX    XX  XX    ',
-'       X  XXXX    XX  XXX   ',
-'    XXXX  XXXXXX  XX  XXXX  ',
-'XXXXXXXX  XXXXXX  XX  XXXX  ']
+'                  YYYYYY',
+' P                Y    Y',
+' ZZZZZXXXXXXXZZYYYYYYYYXXXXZZZZZZZXXXXXXXXXXXXXXXX ',
+'                            ',
+'                            ',
+'                            ',
+'                            ',
+]
 
 
 
-tile_size = 64
+tile_size = 60
 screen_width = 1200
 screen_height = len(level_map) * tile_size
